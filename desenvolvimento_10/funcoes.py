13006bda87a233c4c531f6da9cd2badf98e1fbc2
Paulo-P-33 <luiz.lira029@gmail.com>
2023-11-18 10:26:49 -0300

    desenvolvimento 10 com criacao de funções

---
+++ desenvolvimento_10/funcoes.py
@@ -0,0 +1,18 @@
+def calculadora(num1, num2, operacao):
+  if operacao == "somar": 
+    return num1 + num2
+  elif operacao == "subtrair":
+    return num1 - num2
+  elif operacao == 'multiplicar':
+    return num1 * num2
+  elif operacao == "dividir":
+    if num1 >= num2: 
+      return num1 / num2
+    
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+operacao = input("Digite uma das operações (somar, subtrair, dividir, multiplicar) :")
+
+resultado = calculadora(num1, num2, operacao)
+
+print(f"Você escolheu a operação {operacao} e o resultado é {resultado}")
